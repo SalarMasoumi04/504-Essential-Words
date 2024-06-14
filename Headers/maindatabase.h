4b6093a1eaf7ba4c5611ed1d6034a89d74a4d6f6
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2024-06-14 20:54:35 +0330

    Main Data Base
    Main Data Base

---
+++ Headers/maindatabase.h
@@ -0,0 +1,15 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+#include <QWidget>
+using namespace std;
+
+class MainDataBase: public QWidget
+{
+public:
+
+    static int Number;
+
+    static vector<vector<vector<string>>> words504 ;
+};
+
+#endif // DATABASE_H
