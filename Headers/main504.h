effd31de5aa2df34c7db3ad91b098da89384a471
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2024-06-14 20:52:13 +0330

    Main 504 Header

---
+++ Headers/main504.h
@@ -0,0 +1,27 @@
+#ifndef MAIN504_H
+#define MAIN504_H
+#include "second.h"
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Main504;
+}
+QT_END_NAMESPACE
+
+class Main504 : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Main504(QWidget *parent = nullptr);
+    ~Main504();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::Main504 *ui;
+    Second* second;
+};
+#endif // MAIN504_H
